f95f3135081f25bb298966d703f1d99e47f26025
roou42 <112674105+roou42@users.noreply.github.com>
2025-07-31 14:36:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -384,11 +384,11 @@ class ToRGB(nn.Module):
         if skip is not None:
             skip = self.upsample(skip)
             
-        if skip.shape != out.shape:
-            _, _, h, w = out.shape
-            skip = F.interpolate(skip, size=(h, w), mode='bilinear', align_corners=False)
+            if skip.shape != out.shape:
+                _, _, h, w = out.shape
+                skip = F.interpolate(skip, size=(h, w), mode='bilinear', align_corners=False)
             
-        out = out + skip
+            out = out + skip
 
         return out
 
